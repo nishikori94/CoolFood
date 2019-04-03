064241254f019f5b5d9868fa5a89987cff67b782
nishikori <nishikori94@users.noreply.github.com>
2019-04-03 14:44:07 +0200

    Active orders and history orders added

--- app/src/main/java/com/example/coolfood/OrdersFragment.java
+++ app/src/main/java/com/example/coolfood/OrdersFragment.java
@@ -2,10 +2,16 @@ package com.example.coolfood;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SectionIndexer;
+import android.widget.TableLayout;
+
+import com.example.coolfood.adapter.SectionsPagerAdapter;
 
 
 /**
@@ -13,6 +19,9 @@ import android.view.ViewGroup;
  */
 public class OrdersFragment extends Fragment {
 
+
+    private SectionsPagerAdapter sectionPagerAdapter;
+    private ViewPager viewPager;
 
     public OrdersFragment() {
         // Required empty public constructor
@@ -23,7 +32,22 @@ public class OrdersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders, container, false);
+        View view = inflater.inflate(R.layout.fragment_orders, container, false);
+        sectionPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
+        viewPager = view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        sectionsPagerAdapter.addFragment(new OrdersActiveFragment(), "Active");
+        sectionsPagerAdapter.addFragment(new OrdersHistoryFragment(), "History");
+        viewPager.setAdapter(sectionsPagerAdapter);
     }
 
 }
